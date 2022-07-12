579d8b6c0229dad38b65ab8159184dbff359f65f
Toreole <toreole.official@gmail.com>
2022-07-12 21:18:13 +0200

    Being confused at pointers

--- LearningC++/LearningC++/LearningC++.cpp
+++ LearningC++/LearningC++/LearningC++.cpp
@@ -7,20 +7,44 @@
 
 using namespace std;
 
+int test(int* numberByPointer)
+{
+    //getting the value thats stored at the pointer location requires a * in front of the pointer name.
+    return *numberByPointer + 1;
+}
+
 int main()
 {
-    double x = 0.0;
-    double y = 0.0;
-    double result = 0.0;
-    char oper = '+';
+    //the only way to create a pointer to an int is to allocate it the normal way first, then create the pointer.
+    int bigN = 10;
+    int* bigNptr = &bigN;
 
+    //expected to print 11
+    cout << test(bigNptr) << endl;
+
+    //using new it creates an object in memory, and gives us the address (pointer)
+    Randomizer* randomPtr = new Randomizer();
+    //using a function on the pointer requires an arrow (->)
+    cout << randomPtr->in_range(50, 100) << endl;
+    //manual creation requires manual deletion so we dont cause memory leaks.
+    delete randomPtr;
+
+    //^ manual management discourage in modern C++ apparently. use smart pointers instead, they will automatically release memory
+
+    //what is this exactly? I can clearly call its method, so i assume it is not NULL.
+    //Does this mean that it is automatically created and destroyed at the end of the scope?
     Randomizer random;
 
     for(int i = 0; i < 40; ++i)
         cout << random.in_range(2, 10) << endl;
     cout << endl;
 
+    //-------- CALCULATOR -----------
 
+    double x = 0.0;
+    double y = 0.0;
+    double result = 0.0;
+    char oper = '+';
 
     cout << "Calculator Console Application" << endl << endl;
     cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b" << endl;
